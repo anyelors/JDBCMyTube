dbabe4bf8ae5d4fdc7d966b02b68fe0a6accc0d0
anyelors <anyelors@gmail.com>
2025-10-22 20:23:26 +0200

    Clase JDBC Ajuste #1

--- src/main/java/datasource/Database.java
+++ src/main/java/datasource/Database.java
@@ -9,7 +9,7 @@ public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String BD = "mytube";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "admin"; //1234
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL+BD, USER, PASSWORD);
--- src/main/java/v1/EjemploJDBCDAO.java
+++ src/main/java/v1/EjemploJDBCDAO.java
@@ -7,6 +7,7 @@ import modelo.Usuario;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class EjemploJDBCDAO {
@@ -15,14 +16,25 @@ public class EjemploJDBCDAO {
         List<Usuario> usuarios;
         try (Connection con = Database.getConnection()){
             CrudDAO<Usuario> usuariosDAO = new UsuarioDAO(con);
-            /*
+
             Usuario nuevo = new Usuario("12345678P",
+                    "Lupe Ramirez",
+                    LocalDate.of(1982, 9, 23),
+                    "lupe@mail.com");
+
+            Usuario nuevo1 = new Usuario("12345679R",
                     "Paco Perez",
                     LocalDate.of(1985, 6, 17),
                     "paco@mail.com");
 
+            Usuario nuevo2 = new Usuario("12345680W",
+                    "Pepa Pig",
+                    LocalDate.of(1980, 5, 12),
+                    "paco@mail.com");
+
             usuariosDAO.alta(nuevo);
-             */
+            usuariosDAO.alta(nuevo1);
+            usuariosDAO.alta(nuevo2);
 
             Usuario usuario = usuariosDAO.obtener(2L);
             System.out.println(usuario.toString());
